68cf2ef87a9aa794807522b64155d510a465945e
USER\asus1 <dodi.arahmat@gmail.com>
2025-02-08 14:15:51 +0700

    add sign out feature

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,8 @@ use App\Http\Controllers\AuthController;
 Route::post('sign-up', [AuthController::class, 'signUp']);
 Route::post('sign-in', [AuthController::class, 'signIn']);
 
-Route::middleware('auth_sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function() {
+    Route::post('sign-out', [AuthController::class, 'signOut']);
 });
 
 // Route::get('/user', function (Request $request) {
