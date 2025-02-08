767e969146a47121ac17c45e9e4d6a892663ffa9
USER\asus1 <dodi.arahmat@gmail.com>
2025-02-08 16:44:16 +0700

    add see profile

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Me\ProfileController;
 
 Route::post('sign-up', [AuthController::class, 'signUp']);
 Route::post('sign-in', [AuthController::class, 'signIn']);
 
 Route::middleware('auth:api')->group(function() {
-    Route::middleware(['auth:api', 'jwt.refresh'])->post('refresh', [AuthController::class, 'refresh']);
+    Route::prefix('me')->group(function() {
+        Route::get('profile', [ProfileController::class, 'show']);
+    });
     Route::post('sign-out', [AuthController::class, 'signOut']);
 });
 
